93076ca06e3751c4ced390d2c3a069a67f080955
kaleem <kaleemnisar478@gmail.com>
2021-02-28 15:58:57 +0500

    Initial

---
+++ app/src/main/java/com/seher/shoppingregionlocator/Login.java
@@ -0,0 +1,21 @@
+package com.seher.shoppingregionlocator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void loginUser(View view) {
+    }
+
+    public void forgetPassword(View view) {
+    }
+}
