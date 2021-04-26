67713ded538fa912a94c24ab6f5a31e8c73a7ba2
kaleem <kaleemnisar478@gmail.com>
2021-04-26 14:22:34 +0500

    new additions

---
+++ app/src/main/java/com/seher/shoppingregionlocator/Map/view/PlaceDetailsScreen.java
@@ -0,0 +1,16 @@
+package com.seher.shoppingregionlocator.Map.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.seher.shoppingregionlocator.R;
+
+public class PlaceDetailsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_details_screen);
+    }
+}
